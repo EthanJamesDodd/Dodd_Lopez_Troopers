233dfca70ee2a69be99d1504258f1e6b840b9d5e
Ethan <EthanJamesDodd@gmail.com>
2018-04-16 19:31:08 -0400

    Better stuff eh?

--- js/main.js
+++ js/main.js
@@ -2,12 +2,19 @@
 //Variable Stack
  var canvas = document.getElementById("canvas");
  var ctx = canvas.getContext("2d");
+
 let spawnLineY = 0, //Bugs spawn at y=0
-    spawnRate = 2500, //Bugs spawn ever 2000ms
-    spawnRateOfDescent = 0.50, //How fast the bugs will descend
+    spawnRate = 1250, //Bugs spawn ever 2000ms
+    spawnRateOfDescent = .50, //How fast the bugs will descend
     lastSpawn = -1, //Last bug spawned
     objects = [], //Holds all spawned objects
-    startTime = Date.now(); //Star time, to calculate total time
+    startTime = Date.now(),
+    playerLives = [1, 2, 3],
+    playerImage = document.querySelector('#splatImage'),
+    showScore = document.querySelector('.totalScore'),
+    playState = true,
+    clickCount = 0,
+    finalScore = 0;
 
 const bugOne = new Image();
 bugOne.src = "assets/bug1.svg";
@@ -26,7 +33,7 @@ let bugArray = [bugOne, bugTwo, bugThree];
 
 animate();
 
-function bugSpawn() {
+function bugSpawn() { //Function aiding in where bugs will be spawned, and what type
     // create the new object
     var object = {
         // set x randomly but at least 15px off the canvas edges
@@ -40,47 +47,81 @@ function bugSpawn() {
     objects.push(object);
 }
 
-function animate() {
+function animate() { //Begin Animate
 
-    // get the elapsed time
-    var time = Date.now();
+    var time = Date.now();// get the elapsed time
+    showScore.textContent = finalScore
+
 
     // see if its time to spawn a new object
     if (time > (lastSpawn + spawnRate)) {
         lastSpawn = time;
         bugSpawn();
-        spawnRate*=.98;
+        //spawnRate*=.99;
     }
 
     // clear the canvas so all objects can be
     // redrawn in new positions
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //clearRect, clears old images
 
-    // move each object down the canvas
-    for (var i = 0; i < objects.length; i++) {
-        var object = objects[i];
-        object.y += spawnRateOfDescent;
-        ctx.beginPath();
-        ctx.rect(object.x, object.y, 50, 50);
-        ctx.drawImage(object.image, object.x, object.y, 50, 50);
-        ctx.fillStyle = 'rgba(200, 0, 0, 0)';
-        ctx.fill();
-        ctx.addHitRegion({id: `Bug${i}` });
+    objects.forEach((object, index) => { //Spawn the bugs
+      object.y += spawnRateOfDescent;
+      ctx.beginPath();
+      ctx.rect(object.x, object.y, 50, 50);
+      ctx.drawImage(object.image, object.x, object.y, 50, 50);
+      ctx.fillStyle = 'rgba(0, 0, 0, 0)';
+      ctx.fill();
+      ctx.addHitRegion({id: `Bug${index}` });
+
+      let i = 0;
+      if(objects[index].y == canvas.height){ //Check if bugs hit bottom, if they do remove a life
+
+        playerLives.splice(i, 1);
+        i++;
+      }
+      if(playerLives.length == 0){ //Check when lives hit 0. If they do, lose the game
+        loseGame();
+      }
+    })
+
+    playerLives.forEach((lives, index) => { //Draw life total on Canvas
+      ctx.drawImage(playerImage, 10 + (index*30), 10, 30, 30);
+    })
+
+    if(playState === false){ //Stops animation when you lose
+      window.cancelAnimationFrame(animate);
+      return;
     }
-    // request another animation frame
+
+
     requestAnimationFrame(animate);
+} //End Animate
+
+function loseGame() { //Lose game function, aka lose game screen
+  console.log('you lost')
+  playState = false;
+  objects.splice(0);
+  console.log(objects);
 }
 
-function onClick(){
-  if(event.region) {
-       console.log('it works?');
-       for (var i = 0; i < objects.length; i++) {
-           var object = objects[i];
-           object.y += spawnRateOfDescent;
-           ctx.drawImage(splatImage, object.x, object.y, 200, 200);
-       }
+function onClick() { //Onclick, squash bugs!
+  if(event.region) {//Check if it is in hit region
+       clickCount++;
+
+       objects.forEach((object, index) => {
+         if(event.region == `Bug${index}`) { //If the area clicked is the same hit region. do this
+           console.log(`Bug${index}`)
+           delete objects[index];
+          ctx.drawImage(playerImage, 10, 10, 10, 10)
+           finalScore += 3;
+         }
+         console.log(finalScore);
+       })
      }
 }
+
+
+
 
 canvas.addEventListener('click', onClick);
 
